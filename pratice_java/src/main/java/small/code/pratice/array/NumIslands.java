16f83e37889fa9b8c1e2034fded759587f96a753
Mau <mlam21@dxc.com>
2020-04-18 11:05:37 +0700

    add code

--- pratice_java/src/main/java/small/code/pratice/array/NumIslands.java
+++ pratice_java/src/main/java/small/code/pratice/array/NumIslands.java
@@ -3,8 +3,36 @@ package small.code.pratice.array;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.function.Consumer;
-import java.util.function.Function;
 
+/**
+ * 
+ * Number of Islands
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
+ * An island is surrounded by water and is formed by connecting adjacent lands 
+ * horizontally or vertically. You may assume all four edges of the grid are all
+ *  surrounded by water.
+ *  
+ *  Example 1:
+ *  
+ *  Input:
+ *  11110
+ *  11010
+ *  11000
+ *  00000
+ *  
+ *  Output: 1
+ *  
+ *  Example 2:
+ *  
+ *  Input:
+ *  11000
+ *  11000
+ *  00100
+ *  00011
+ *  
+ *  Output: 3
+ *
+ */
 public class NumIslands {
 	private static char PASSED = '2', LAND = '1';
 	static class Point {
